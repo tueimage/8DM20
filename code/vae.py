e87ace1ca600c99f21ac8b197ca418217cdd497d
Cian Scannell <cianmscannell@gmail.com>
2026-03-05 19:18:19 +0100

    VAE code

---
+++ code/vae.py
@@ -0,0 +1,302 @@
+import torch
+import torch.nn as nn
+
+l1_loss = torch.nn.L1Loss()
+
+
+class Block(nn.Module):
+    """Basic convolutional building block
+
+    Parameters
+    ----------
+    in_ch : int
+        number of input channels to the block
+    out_ch : int     
+        number of output channels of the block
+    """
+
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_ch, out_ch, 3, padding=1)
+        self.bn1 = # TODO   # batch normalisation
+        self.conv2 = nn.Conv2d(out_ch, out_ch, 3, padding=1)
+        self.bn2 = # TODO 
+        self.lrelu =  # TODO  # leaky ReLU
+
+
+    def forward(self, x):
+        """Performs a forward pass of the block
+       
+        x : torch.Tensor
+            the input to the block
+        torch.Tensor
+            the output of the forward pass
+        """
+        # a block consists of two convolutional layers
+        # with ReLU activations
+        # use batch normalisation
+
+        # TODO
+        return x
+
+
+class Encoder(nn.Module):
+    """The encoder part of the VAE.
+
+    Parameters
+    ----------
+    spatial_size : list[int]
+        size of the input image, by default [64, 64]
+    z_dim : int
+        dimension of the latent space
+    chs : tuple
+        hold the number of input channels for each encoder block
+    """
+
+    def __init__(self, spatial_size=[64, 64], z_dim=256, chs=(1, 64, 128, 256)):
+        super().__init__()
+        # convolutional blocks
+        self.enc_blocks = nn.ModuleList(
+            # TODO
+        )
+        # max pooling
+        self.pool = # TODO
+        # height and width of images at lowest resolution level
+        _h, _w = # TODO
+
+        # flattening
+        self.out = nn.Sequential(nn.Flatten(1), nn.Linear(chs[-1] * _h * _w, 2 * z_dim))
+
+    def forward(self, x):
+        """Performs the forward pass for all blocks in the encoder.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            input image to the encoder
+
+        Returns
+        -------
+        list[torch.Tensor]    
+            a tensor with the means and a tensor with the log variances of the
+            latent distribution
+        """
+
+        for block in self.enc_blocks:
+            # TODO: conv block           
+            # TODO: pooling 
+        # TODO: output layer          
+        return torch.chunk(x, 2, dim=1)  # 2 chunks, 1 each for mu and logvar
+
+
+class Generator(nn.Module):
+    """Generator of the VAE
+
+    Parameters
+    ----------
+    z_dim : int 
+        dimension of latent space
+    chs : tuple
+        holds the number of channels for each block
+    h : int, optional
+        height of image at lowest resolution level, by default 8
+    w : int, optional
+        width of image at lowest resolution level, by default 8    
+    """
+
+    def __init__(self, z_dim=256, chs=(256, 128, 64, 32), h=8, w=8):
+
+        super().__init__()
+        self.chs = chs
+        self.h = h  
+        self.w = w  
+        self.z_dim = z_dim  
+        self.proj_z = nn.Linear(
+            self.z_dim, self.chs[0] * self.h * self.w
+        )  # fully connected layer on latent space
+        self.reshape = lambda x: torch.reshape(
+            x, (-1, self.chs[0], self.h, self.w)
+        )  # reshaping
+
+        self.upconvs = nn.ModuleList(
+            # TODO: transposed convolution            
+        )
+
+        self.dec_blocks = nn.ModuleList(
+            # TODO: conv block           
+        )
+        self.head = nn.Sequential(
+            nn.Conv2d(self.chs[-1], 1, kernel_size=3, padding=1),
+            nn.Tanh(),
+        )  # output layer
+
+    def forward(self, z):
+        """Performs the forward pass of generator
+
+        Parameters
+        ----------
+        z : torch.Tensor
+            input to the generator
+        
+        Returns
+        -------
+        x : torch.Tensor
+        
+        """
+        x = # TODO: fully connected layer
+        x = # TODO: reshape to image dimensions
+        for i in range(len(self.chs) - 1):
+            # TODO: transposed convolution
+            # TODO: convolutional block
+        return self.head(x)
+
+
+class VAE(nn.Module):
+    """A representation of the VAE
+
+    Parameters
+    ----------
+    enc_chs : tuple 
+        holds the number of input channels of each block in the encoder
+    dec_chs : tuple 
+        holds the number of input channels of each block in the decoder
+    """
+    def __init__(
+        self,
+        enc_chs=(1, 64, 128, 256),
+        dec_chs=(256, 128, 64, 32),
+    ):
+        super().__init__()
+        self.encoder = Encoder()
+        self.generator = Generator()
+
+
+    def forward(self, x):
+        """Performs a forwards pass of the VAE and returns the reconstruction
+        and mean + logvar.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            the input to the encoder
+
+        Returns
+        -------
+        torch.Tensor
+            the reconstruction of the input image
+        float
+            the mean of the latent distribution
+        float
+            the log of the variance of the latent distribution
+        """
+        mu, logvar = self.encoder(x)
+        latent_z = sample_z(mu, logvar)
+        
+        output = self.generator(latent_z)
+        
+        return output, mu, logvar
+
+
+def get_noise(n_samples, z_dim, device="cpu"):
+    """Creates noise vectors.
+    
+    Given the dimensions (n_samples, z_dim), creates a tensor of that shape filled with 
+    random numbers from the normal distribution.
+
+    Parameters
+    ----------
+    n_samples : int
+        the number of samples to generate
+    z_dim : int
+        the dimension of the noise vector
+    device : str
+        the type of the device, by default "cpu"
+    """
+    return torch.randn(n_samples, z_dim, device=device)
+
+
+def sample_z(mu, logvar):
+    """Samples noise vector from a Gaussian distribution with reparameterization trick.
+
+    Parameters
+    ----------
+    mu : float
+        the mean of the distribution
+    logvar : float
+        the log of the variance of the distribution
+    """
+    eps = torch.randn(mu.size(), device=mu.device).to(mu.dtype)
+    return (logvar / 2).exp() * eps + mu
+
+
+def kld_loss(mu, logvar):
+    """Computes the KLD loss given parameters of the predicted 
+    latent distribution.
+
+    Parameters
+    ----------
+    mu : float
+        the mean of the distribution
+    logvar : float
+        the log of the variance of the distribution
+
+    Returns
+    -------
+    float
+        the kld loss
+
+    """
+    return -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp())
+
+def vae_loss(inputs, recons, mu, logvar, beta=1):
+    """Computes the VAE loss, sum of reconstruction and KLD loss
+
+    Parameters
+    ----------
+    inputs : torch.Tensor
+        the input images to the vae
+    recons : torch.Tensor
+        the predicted reconstructions from the vae
+    mu : float
+        the predicted mean of the latent distribution
+    logvar : float
+        the predicted log of the variance of the latent distribution
+
+    Returns
+    -------
+    float
+        sum of reconstruction and KLD loss
+    """
+    return l1_loss(inputs, recons) + beta*kld_loss(mu, logvar)
+
+class Discriminator(nn.Module):
+    def __init__(self, ndf):
+        super(Discriminator, self).__init__()
+
+        self.main = nn.Sequential(
+            # input is (nc) x 64 x 64
+            nn.Conv2d(1, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 32 x 32
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 16 x 16
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 8 x 8
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x 4 x 4
+            # nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Flatten(1, -1),
+            nn.Linear(8192, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, input):
+        return self.main(input)
